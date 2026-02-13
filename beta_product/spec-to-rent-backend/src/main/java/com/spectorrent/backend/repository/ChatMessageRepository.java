2834c04c6b5e05e8f9a3cc79be4a0b541f35b167
ileztom <91593668+ileztom@users.noreply.github.com>
2026-02-13 17:25:49 +0300

    Delete beta_product directory

--- beta_product/spec-to-rent-backend/src/main/java/com/spectorrent/backend/repository/ChatMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spectorrent.backend.repository;
-
-import com.spectorrent.backend.domain.ChatMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-}
--- beta_product/spec-to-rent-backend/src/main/java/com/spectorrent/backend/repository/RentalItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spectorrent.backend.repository;
-
-import com.spectorrent.backend.domain.RentalItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentalItemRepository extends JpaRepository<RentalItem, Long> {
-}
