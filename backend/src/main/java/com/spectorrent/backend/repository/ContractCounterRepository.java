8985d588d512903fe41312acf0a53602e6ab49c8
ileztom <91593668+ileztom@users.noreply.github.com>
2026-06-15 15:34:26 +0300

    Delete backend directory

--- backend/src/main/java/com/spectorrent/backend/repository/ContractCounterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spectorrent.backend.repository;
-
-import com.spectorrent.backend.domain.ContractCounter;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContractCounterRepository extends JpaRepository<ContractCounter, Long> {
-}
